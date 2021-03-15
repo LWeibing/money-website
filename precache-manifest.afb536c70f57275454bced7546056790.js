e0077d94afc8182dac9a3fe80fccc152cdcde5f3
Clown <1104480806@qq.com>
2021-03-15 22:23:07 +0800

    deploy

---
+++ precache-manifest.afb536c70f57275454bced7546056790.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5f81dcef5718d65b3ed",
+    "url": "/money-website-vue/css/app.32c4bab8.css"
+  },
+  {
+    "revision": "db1466cbfdbba48a06a0b8950609949a",
+    "url": "/money-website-vue/img/qrcode.db1466cb.png"
+  },
+  {
+    "revision": "1c4f22326e669e287004f03612de597a",
+    "url": "/money-website-vue/index.html"
+  },
+  {
+    "revision": "d5f81dcef5718d65b3ed",
+    "url": "/money-website-vue/js/app.16a9df62.js"
+  },
+  {
+    "revision": "bcab6f3898345db03ddb",
+    "url": "/money-website-vue/js/chunk-vendors.52913757.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/money-website-vue/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-website-vue/robots.txt"
+  }
+]);
